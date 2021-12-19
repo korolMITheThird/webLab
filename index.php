26a9a735784906c83100195fbc0edbf537411ad5
korolMITheThird <96379432+korolMITheThird@users.noreply.github.com>
2021-12-19 18:42:12 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
     $tmp=file_get_contents("main.tpl");
-    $preloader=file_get_contents("html/preloader.html");
-    $header=file_get_contents("html/header.html");
-    $slide1=file_get_contents("html/slide1.html");
-    $slide2=file_get_contents("html/slide2.html");
-    $slide3=file_get_contents("html/slide3.html");
-    $slide4=file_get_contents("html/slide4.html");
-    $slide5=file_get_contents("html/slide5.html");
+    $preloader=file_get_contents("preloader.html");
+    $header=file_get_contents("header.html");
+    $slide1=file_get_contents("slide1.html");
+    $slide2=file_get_contents("slide2.html");
+    $slide3=file_get_contents("slide3.html");
+    $slide4=file_get_contents("slide4.html");
+    $slide5=file_get_contents("slide5.html");
     $tmp = str_replace("{preloader}", $preloader, $tmp);
     $tmp = str_replace("{header}", $header, $tmp);
     $tmp = str_replace("{slide1}", $slide1, $tmp);
